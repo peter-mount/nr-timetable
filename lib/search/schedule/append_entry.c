67015d275d82798b521620e405415eebb5d5d475
Peter Mount <peter@phoebe.area51.onl>
2016-08-15 09:18:41 +0100

    Time

--- lib/search/schedule/append_entry.c
+++ lib/search/schedule/append_entry.c
@@ -24,17 +24,70 @@ void tt_append_scheduleEntry(struct charbuffer *b, struct ScheduleEntry *e) {
         json_append_int(b, (int) e->tiploc);
         //json_append_str(b, e->tiploc);
 
-        charbuffer_append(b, ",\"tiplocSeq\":\"");
-        charbuffer_add(b, e->tiplocseq);
-        charbuffer_add(b, '"');
+        if (e->tiplocseq && e->tiplocseq != ' ') {
+            charbuffer_append(b, ",\"tiplocSeq\":\"");
+            charbuffer_add(b, e->tiplocseq);
+            charbuffer_add(b, '"');
+        }
 
-        append_hhmm(b, "pta", e->pta);
-        append_hhmm(b, "ptd", e->ptd);
+        if (e->pta) append_hhmm(b, "pta", e->pta);
+        if (e->ptd) append_hhmm(b, "ptd", e->ptd);
 
-        append_hhmmss(b, "wta", e->wta);
-        append_hhmmss(b, "wtd", e->wtd);
-        append_hhmmss(b, "wtp", e->wtp);
-        
+        if (e->wta) append_hhmmss(b, "wta", e->wta);
+        if (e->wtd) append_hhmmss(b, "wtd", e->wtd);
+        if (e->wtp) append_hhmmss(b, "wtp", e->wtp);
+
+        if (e->platform[0]) {
+            charbuffer_append(b, ",\"platform\":");
+            json_append_str(b, e->platform);
+        }
+
+        if (e->line[0]) {
+            charbuffer_append(b, ",\"line\":");
+            json_append_str(b, e->line);
+        }
+
+        if (e->path[0]) {
+            charbuffer_append(b, ",\"path\":");
+            json_append_str(b, e->path);
+        }
+
+        // activity
+        char ac = 0;
+        for (int i = 0; i < 6; i++) {
+            if (e->activity[i][0]) {
+                if (ac)
+                    charbuffer_add(b, ac);
+                else {
+                    charbuffer_append(b, ",\"activity\":[");
+                    ac = ',';
+                }
+                json_append_str(b, e->activity[i]);
+            }
+        }
+        if (ac)
+            charbuffer_add(b, ']');
+
+        if (e->engAllow[0]) {
+            charbuffer_append(b, ",\"engAllow\":");
+            json_append_str(b, e->engAllow);
+        }
+
+        if (e->pathAllow[0]) {
+            charbuffer_append(b, ",\"pathAllow\":");
+            json_append_str(b, e->pathAllow);
+        }
+
+        if (e->perfAllow[0]) {
+            charbuffer_append(b, ",\"perfAllow\":");
+            json_append_str(b, e->perfAllow);
+        }
+
+        if (e->resThameslink[0]) {
+            charbuffer_append(b, ",\"resThameslink\":");
+            json_append_str(b, e->resThameslink);
+        }
+
         charbuffer_add(b, '}');
     } else {
         charbuffer_append(b, "{}");
--- lib/search/schedule/append_schedule.c
+++ lib/search/schedule/append_schedule.c
@@ -13,14 +13,51 @@
 
 extern struct TimeTable *timetable;
 
+void json_append_date_ISO(struct charbuffer *b, time_t *t) {
+    struct tm tm;
+    if (t && *t) {
+        gmtime_r(t, &tm);
+        charbuffer_printf(b, "\"%04d-%02d-%02d\"", 1900 + tm.tm_year, 1 + tm.tm_mon, tm.tm_mday);
+    } else
+        json_append_str(b, "null");
+}
+
+void json_append_time_hhmm(struct charbuffer *b, time_t *t) {
+    struct tm tm;
+    if (t && *t) {
+        gmtime_r(t, &tm);
+        charbuffer_printf(b, "\"%02d:%02d\"", tm.tm_hour, tm.tm_min);
+    } else
+        json_append_str(b, "null");
+}
+
+void json_append_time_hhmmss(struct charbuffer *b, time_t *t) {
+    struct tm tm;
+    if (t && *t) {
+        gmtime_r(t, &tm);
+        charbuffer_printf(b, "\"%02d:%02d:%02d\"", tm.tm_hour, tm.tm_min, tm.tm_sec);
+    } else
+        json_append_str(b, "null");
+}
+
+void json_append_datetime_ISO(struct charbuffer *b, time_t *t) {
+    struct tm tm;
+    if (t && *t) {
+        gmtime_r(t, &tm);
+        charbuffer_printf(b, "\"%04d-%02d-%02dT%02d:%02d:%02dZ\"",
+                1900 + tm.tm_year, 1 + tm.tm_mon, tm.tm_mday,
+                tm.tm_hour, tm.tm_min, tm.tm_sec);
+    } else
+        json_append_str(b, "null");
+}
+
 void tt_append_schedule(struct charbuffer *b, struct Schedule *e) {
     if (e) {
         charbuffer_append(b, "{\"uid\":");
         json_append_str(b, e->id.uid);
 
         charbuffer_append(b, ",\"start\":");
-        // date from time_t e->id.start
-        charbuffer_append(b, "null");
+        json_append_date_ISO(b, &e->id.start);
 
         charbuffer_append(b, ",\"stdInd\":\"");
         charbuffer_add(b, e->id.stpInd);
@@ -28,16 +65,45 @@ void tt_append_schedule(struct charbuffer *b, struct Schedule *e) {
 
         // ---
 
+        charbuffer_append(b, ",\"end\":");
+        json_append_date_ISO(b, &e->end);
+
+        // daysRun
+        charbuffer_append(b, ",\"daysRunV\":[]");
+
+        charbuffer_append(b, ",\"daysRun\":\"");
+        if (e->daysRun)
+            charbuffer_add(b, e->daysRun);
+        charbuffer_add(b, '"');
+
+        charbuffer_append(b, ",\"bankHolRun\":\"");
+        if (e->bankHolRun)
+            charbuffer_add(b, e->bankHolRun);
+        charbuffer_add(b, '"');
+
+        charbuffer_append(b, ",\"status\":");
+        json_append_str(b, e->status);
+
+        charbuffer_append(b, ",\"category\":");
+        json_append_str(b, e->category);
+
+        charbuffer_append(b, ",\"headcode\":");
+        json_append_str(b, e->headcode);
+
+        charbuffer_append(b, ",\"serviceCode\":");
+        json_append_str(b, e->serviceCode);
+
+        charbuffer_append(b, ",\"portionId\":");
+        json_append_str(b, e->portionId);
+
         // Schedule entries
         charbuffer_append(b, ",\"entries\":");
         charbuffer_add(b, '[');
-        /*
-                for (int i = 0; i < e->numEntries; i++) {
-                    if (i > 0)
-                        charbuffer_add(b, ',');
-                    tt_append_scheduleEntry(b, &e->entries[i]);
-                }
-         */
+        for (int i = 0; i < e->numEntries; i++) {
+            if (i > 0)
+                charbuffer_add(b, ',');
+            tt_append_scheduleEntry(b, &e->entries[i]);
+        }
         charbuffer_add(b, ']');
 
         charbuffer_add(b, '}');
